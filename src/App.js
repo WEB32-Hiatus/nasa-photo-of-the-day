b7839a48217ab675fbd46b783875db709a75c95d
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-06-12 12:27:53 -0700

    added styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,28 @@ import Title from './Components/Title';
 import Image from './Components/Image';
 import Explanation from './Components/Explanation';
 import Date from './Components/Date';
-import Copyright from './Components/Copyright';
+import styled from 'styled-components';
 
+const ContainerWrapper = styled.div`
+  background-color: #191970;
+`;
+
+const TitleWrapper = styled.h1`
+  color: #DC143C;
+`;
+
+const ImageWrapper = styled.image`
+  border-radius: 50%;
+  border: solid red 2px;
+`;
+
+const ExplanationWrapper = styled.p`
+  color: #00FFFF;
+`;
+
+const DateWrapper = styled.p`
+  color: #00FFFF;
+`;
 
 function App() {
 
@@ -15,6 +35,7 @@ function App() {
   useEffect(function() {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=4pZJJ7Fssrfy5cFV4xz2SksnJJwV13ZoMdnc4KW3').then(function(response) {
       setNasaData(response.data)
+      console.log(response.data)
     })
     .catch(function(error) {
       console.log('error', error);
@@ -22,13 +43,22 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Title title={nasaData.title}/>
-      <Image src={nasaData.url} alt={nasaData.explanation}/>
-      <Explanation explanation={nasaData.explanation}/>
-      <Date date={nasaData.date}/>
-      <Copyright copyright={nasaData.copyright}/>
-    </div>
+    <ContainerWrapper>
+      <div className="App">
+        <TitleWrapper>
+          <Title title={nasaData.title}/>
+        </TitleWrapper>
+        <ImageWrapper>
+          <Image src={nasaData.url} alt={nasaData.explanation}/>
+        </ImageWrapper>
+        <ExplanationWrapper>
+          <Explanation explanation={nasaData.explanation}/>
+        </ExplanationWrapper>
+        <DateWrapper>
+          <Date date={nasaData.date}/>
+        </DateWrapper>
+      </div>
+    </ContainerWrapper>
   );
 }
 
--- src/Components/Copyright.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Copyright(props){
-    return (<p>Copyright: {props.copyright}</p>)
-}
